63347ca2dac2cb22ec518a423b4508600b8abede
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-09-05 19:30:59 +0200

    feat: Add initial files and configurations for home and navbar components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Sylvie Molinatto'
+    }
+];
+
+export default routes;
