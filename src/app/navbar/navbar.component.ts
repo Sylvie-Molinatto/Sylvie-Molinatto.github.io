3a1b744d3f262c89bbbd793da6f33b613a1900ea
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-09-07 18:13:49 +0200

    Refactor CSS and HTML files for header and navbar components

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgbDropdownModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  selectedLanguage: string = 'gb';
+  languageMap: { [key: string]: string } = {
+    'gb': 'English',
+    'it': 'Italiano'
+  };
 
+  selectLanguage(language: string) {
+    console.log('Selected language: ', language);
+    this.selectedLanguage = language;
+  }
+
+  getLanguageName(): string {
+    return this.languageMap[this.selectedLanguage];
+  }
 }
