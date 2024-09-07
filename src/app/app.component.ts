f1c1377bd1fe6cc68fd037aa37f3eac8afbcf8db
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-09-07 19:08:24 +0200

    feat: Add CSS and HTML files for footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, HomeComponent, RouterModule],
+  imports: [NavbarComponent, HomeComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
