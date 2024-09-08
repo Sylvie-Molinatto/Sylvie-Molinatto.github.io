ee2f67bab9ba15a7af2e33b5b97a782103b18d0e
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-09-08 18:54:46 +0200

    Start color theme initialization and add theme switcher

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgbDropdownModule],
+  imports: [CommonModule, NgbDropdownModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   selectedLanguage: string = 'gb';
+  selectedTheme: 'light' | 'dark' = 'light';  
   languageMap: { [key: string]: string } = {
     'gb': 'English',
     'it': 'Italiano'
@@ -22,5 +24,10 @@ export class NavbarComponent {
 
   getLanguageName(): string {
     return this.languageMap[this.selectedLanguage];
+  }
+
+  toggleTheme() {
+    this.selectedTheme = this.selectedTheme === 'light' ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', this.selectedTheme);
   }
 }
