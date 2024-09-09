c76ccffc2484c0c2080ea7d7dbf023ca9699c120
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-09-09 15:33:37 +0200

    Refactor asset paths in angular.json and package.json and add ngx-translate/core

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,38 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import routes from './app.routes';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import routes from './app.routes';
+
+// Funzione per creare il loader di traduzioni
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    },
+    TranslateService,
+    ...TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }).providers || []
+  ]
 };
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, NgbDropdownModule],
+  imports: [CommonModule, NgbDropdownModule, TranslateModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   selectedLanguage: string = 'gb';
   selectedTheme: 'light' | 'dark' = 'light';  
+  
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang(this.selectedLanguage);
+  }
+
   languageMap: { [key: string]: string } = {
     'gb': 'English',
     'it': 'Italiano'
   };
 
   selectLanguage(language: string) {
-    console.log('Selected language: ', language);
     this.selectedLanguage = language;
+    this.translate.use(language);
   }
 
   getLanguageName(): string {
