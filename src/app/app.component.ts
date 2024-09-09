1553c118119b3d5dc4ad174060e6c90a48e75741
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-09-09 15:48:10 +0200

    Move language and theme settings into app.component.ts, translate also footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,40 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [NavbarComponent, HomeComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'portfolio-website';
+  selectedLanguage: string = 'gb';
+  selectedTheme: 'light' | 'dark' = 'light';
+
+  languageMap: { [key: string]: string } = {
+    'gb': 'English',
+    'it': 'Italiano'
+  };
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang(this.selectedLanguage);
+  }
+
+  selectLanguage(language: string) {
+    this.selectedLanguage = language;
+    this.translate.use(language);
+  }
+
+  getLanguageName(): string {
+    return this.languageMap[this.selectedLanguage];
+  }
+
+  toggleTheme() {
+    this.selectedTheme = this.selectedTheme === 'light' ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', this.selectedTheme);
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,39 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
-import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, NgbDropdownModule, TranslateModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  selectedLanguage: string = 'gb';
-  selectedTheme: 'light' | 'dark' = 'light';  
-  
-  constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang(this.selectedLanguage);
-  }
-
-  languageMap: { [key: string]: string } = {
-    'gb': 'English',
-    'it': 'Italiano'
-  };
-
-  selectLanguage(language: string) {
-    this.selectedLanguage = language;
-    this.translate.use(language);
-  }
-
-  getLanguageName(): string {
-    return this.languageMap[this.selectedLanguage];
-  }
-
-  toggleTheme() {
-    this.selectedTheme = this.selectedTheme === 'light' ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-theme', this.selectedTheme);
-  }
+  @Input() selectedLanguage!: string;
+  @Input() selectedTheme!: 'light' | 'dark';
+  @Input() languageMap!: { [key: string]: string };
+  @Input() selectLanguage!: (language: string) => void;
+  @Input() getLanguageName!: () => string;
+  @Input() toggleTheme!: () => void;
 }
