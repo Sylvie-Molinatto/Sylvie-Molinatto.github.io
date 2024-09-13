0119123609273520bf1e8929f85ddbaff3381055
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-09-13 16:27:02 +0200

    Refactor navbar and sidenav components

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject, TemplateRef } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
-import { SidenavComponent } from '../sidenav/sidenav.component';
+import { NgbOffcanvasModule, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, NgbDropdownModule, TranslateModule, RouterModule, SidenavComponent],
+  imports: [CommonModule, NgbDropdownModule, TranslateModule, RouterModule, NgbOffcanvasModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
@@ -19,14 +19,12 @@ export class NavbarComponent{
   @Input() selectLanguage!: (language: string) => void;
   @Input() getLanguageName!: () => string;
   @Input() toggleTheme!: () => void;
+  
 
-  isSidenavOpen = false;
+  private offcanvasService = inject(NgbOffcanvas);
+	closeResult = '';
 
-  openSidenav() {
-    this.isSidenavOpen = true;
-  }
-
-  closeSidenav() {
-    this.isSidenavOpen = false;
-  }
+	open(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title', position: 'end' });
+	}
 }
