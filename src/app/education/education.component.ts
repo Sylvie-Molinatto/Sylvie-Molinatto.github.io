a10f0afb6d48dc1830a3cd8c6216d9c4f2e7efb1
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-09-17 10:46:26 +0200

    Upgrade about me and education pages

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './education.component.html',
-  styleUrl: './education.component.scss'
+  styleUrls: ['./education.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EducationComponent {
+  events: Array<any>;
 
+  constructor(private translate: TranslateService) {
+    this.events = new Array();
+    this.loadEvents();
+
+    // Sottoscrivi agli eventi di cambio lingua
+    this.translate.onLangChange.subscribe(() => {
+      this.loadEvents();
+    });
+  }
+
+  loadEvents() {
+    this.translate.get(['EDU.MASTER', 'EDU.BACHELOR', 'EDU.HIGH_SCHOOL', 'EDU.PRESENT']).subscribe(translations => {
+      this.events = [
+        {
+          startYear: '2022',
+          endYear: translations['EDU.PRESENT'],
+          title: translations['EDU.MASTER'],
+          location: 'Politecnico di Torino',
+          description: '',
+          logo: 'Logo_PoliTo.png',
+          color: '#5C3D3Fff',
+          icon: 'pi pi-spinner-dotted'
+        },
+        {
+          startYear: '2019',
+          endYear: '2022',
+          title: translations['EDU.BACHELOR'],
+          location: 'Politecnico di Torino',
+          description: '',
+          logo: 'Logo_PoliTo.png',
+          color: '#FA8993ff',
+          icon: 'pi pi-check',
+          grade: '96/110'
+        },
+        {
+          startYear: '2014',
+          endYear: '2019',
+          title: translations['EDU.HIGH_SCHOOL'],
+          location: 'Liceo Scientifico Statale A. Volta',
+          description: '',
+          logo: '',
+          color: '#FA8993ff',
+          icon: 'pi pi-check',
+          grade: '90/100'
+        }
+      ];
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
